b9dcca9b8ded5b88f7a9e076cfbec4b0b11acebf
Eusebiu Timofte <timoftesebi98@gmail.com>
2020-12-17 21:00:23 +0200

    issue state comment like

---
+++ CivicHub/Entities/IssueStateCommentLike.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CivicHub.Entities
+{
+    public class IssueStateCommentLike: BaseEntity
+    {
+        public Guid IssueStateCommentId { get; set; }
+        public IssueStateComment IssueStateComment { get; set; }
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+    }
+}
