96205ef45cd6ac0a5a07e11b4f768be2633ea222
Andrei <andrei131298@yahoo.com>
2020-12-18 13:06:43 +0200

    input validation

--- CivicHub/ClientApp/src/app/petition-profile/sign-form/sign-form.component.ts
+++ CivicHub/ClientApp/src/app/petition-profile/sign-form/sign-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ModalDirective } from 'ngx-bootstrap/modal';
+import { ApiService } from 'src/app/shared/api.service';
 
 @Component({
   selector: 'sign-form',
@@ -8,11 +10,69 @@ import { ModalDirective } from 'ngx-bootstrap/modal';
 })
 export class SignFormComponent implements OnInit {
 
-  constructor() { }
+  addSignForm: FormGroup;
+  success: boolean;
+
+  constructor(private fb:FormBuilder, private api:ApiService) { }
 
   @ViewChild("signForm") signForm: ModalDirective;
   
-  ngOnInit() {}
+  validateAllFormFields(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach((field) => {
+        const control = formGroup.get(field);
+        if (control instanceof FormControl) {
+            control.markAsTouched({ onlySelf: true });
+        } else if (control instanceof FormGroup) {
+            this.validateAllFormFields(control);
+        }
+    });
+}
+
+  get f() {
+    return this.addSignForm.controls;
+}
+
+  ngOnInit() {
+    this.addSignForm = this.fb.group({
+      firstName: [null, Validators.required],
+      lastName: [null, Validators.required],
+      cnp: [null, Validators.required],
+      address: [null, Validators.required],
+      idSeries: [null, Validators.required],
+      idNumber: [null, Validators.required],
+  });
+  }
+
+  addSign() {
+    if (this.addSignForm.valid) {
+        this.success = true;
+        setTimeout(() => {
+            this.success = null;
+        }, 3000);
+        console.log("loginForm submitted");
+        console.log(this.f);
+        //api add
+    } else {
+        this.success = false;
+        setTimeout(() => {
+            this.success = null;
+        }, 3000);
+        this.validateAllFormFields(this.addSignForm);
+    }
+    
+}
+isFieldValid(field: string) {
+  return (
+    !this.addSignForm.get(field).valid && this.addSignForm.get(field).touched
+  );
+}
+
+displayFieldCss(field: string) {
+  return {
+    "has-error": this.isFieldValid(field),
+    "has-feedback": this.isFieldValid(field),
+  };
+}
 
   initialize(): void {
     this.signForm.show();
