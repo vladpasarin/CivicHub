7ab13f6dde903c89f33d3c8e43b4170dcc1792d7
Eusebiu Timofte <timoftesebi98@gmail.com>
2021-01-16 23:57:56 +0200

    number of signatures per issue get

---
+++ CivicHub/Repositories/PrizeGivenRepository.cs
@@ -0,0 +1,17 @@
+﻿using CivicHub.Data;
+using CivicHub.Entities;
+using CivicHub.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CivicHub.Repositories
+{
+    public class PrizeGivenRepository : GenericRepository<PrizeGiven> , IPrizeGivenRepository
+    {
+        public PrizeGivenRepository(Context context) : base(context)
+        {
+        }
+    }
+}
---
+++ CivicHub/Repositories/PrizeRepository.cs
@@ -0,0 +1,17 @@
+﻿using CivicHub.Data;
+using CivicHub.Entities;
+using CivicHub.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CivicHub.Repositories
+{
+    public class PrizeRepository : GenericRepository<Prize>, IPrizeRepository
+    {
+        public PrizeRepository(Context context) : base(context)
+        {
+        }
+    }
+}
