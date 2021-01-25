9613b1961af70f063c37de3b9194dbd3b90a598d
Eusebiu Timofte <timoftesebi98@gmail.com>
2021-01-25 20:48:52 +0200

    delete follow by user and issue

--- CivicHub/IServices/IFollowService.cs
+++ CivicHub/IServices/IFollowService.cs
@@ -16,6 +16,7 @@ namespace CivicHub.IServices
         Tuple<int, object> GetByUserId(Guid id);
         Tuple<int, object> GetByIssueId(Guid id);
         Tuple<int, object> GetByIssueAndUserId(Guid userId, Guid issueId);
+        Tuple<int, object> Delete(Guid userId, Guid issueId);
 
     }
 }
